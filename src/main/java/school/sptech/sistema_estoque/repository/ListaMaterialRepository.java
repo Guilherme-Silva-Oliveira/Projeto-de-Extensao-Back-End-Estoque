797096b8b04e4d1ec1a943cd51d9b4a1832a56da
Gabriel Furtado <gabriel.dsousa@sptech.school>
2026-08-04 22:32:16 -0300

    Feat: endpoint get material-mais-solicitado

--- src/main/java/school/sptech/sistema_estoque/repository/ListaMaterialRepository.java
+++ src/main/java/school/sptech/sistema_estoque/repository/ListaMaterialRepository.java
@@ -1,12 +1,33 @@
 package school.sptech.sistema_estoque.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import school.sptech.sistema_estoque.dto.estoque.dashboard.MaterialMaisSolicitadoDto;
 import school.sptech.sistema_estoque.model.estoque.AlertaDevolucao;
 import school.sptech.sistema_estoque.model.estoque.ListaMaterial;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface ListaMaterialRepository extends JpaRepository<ListaMaterial, Integer> {
     List<Optional<ListaMaterial>> findAllBySolicitacaoId(Integer solicitacaoId);
+
+     @Query("""
+        SELECT new school.sptech.sistema_estoque.dto.estoque.dashboard.MaterialMaisSolicitadoDto(
+            lm.material.nomeMaterial,
+            SUM(CAST(lm.quantidade AS long))
+        )
+        FROM ListaMaterial lm
+        WHERE lm.solicitacao.dataSolicitacao BETWEEN :dataInicio AND :dataFim
+        GROUP BY lm.material.nomeMaterial
+        ORDER BY SUM(lm.quantidade) DESC
+    """)
+    List<MaterialMaisSolicitadoDto> findMaterialMaisSolicitadoPorPeriodo(
+            @Param("dataInicio") LocalDateTime dataInicio,
+            @Param("dataFim") LocalDateTime dataFim,
+            Pageable pageable
+    );
 }
